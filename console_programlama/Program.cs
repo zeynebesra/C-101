0c4820d0d13d8da1125f7d67f84fc56e5fc9e240
Zeynebesra <zeynep.ozturk@mobilion.com.tr>
2022-01-13 20:20:04 +0300

    first commit

---
+++ console_programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhabalar!");
+            Console.WriteLine("Adınızı giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin  " + name +" "+surname);
+        }
+    }
+}
