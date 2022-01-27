0628aaf355d44c691592ca9599829a3021592ad5
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-01-27 17:43:39 +0300

    if_else_ternary eklendi.

---
+++ if_else_ternary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace if_else_ternary
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int time = DateTime.Now.Hour;
+
+      if (time >= 6 && time < 11)
+      {
+        Console.WriteLine("Good Morning");
+      }
+      else if (time <= 18)
+      {
+        Console.WriteLine("Good Afternoon");
+      }
+      else
+      {
+        Console.WriteLine("Good Night");
+      }
+
+      string result = time <= 18 ? "Good Afternoon" : "Good Night";
+
+      Console.WriteLine(result);
+    }
+  }
+}
