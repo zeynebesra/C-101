24da9be4812de787eb2df80bcf7d38b5cd1a3e56
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-01-27 17:30:18 +0300

    Try Catch eklendi.

---
+++ try_catch/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace try_catch
+{
+    
+    class Program
+    {
+       
+       static void Main(string[] args)
+       {
+         try
+         {
+             Console.WriteLine("Bir sayı giriniz :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz sayı : " + a);
+         }
+
+         catch(Exception ex)
+         {
+           Console.WriteLine(ex.Message);
+         }
+         finally
+         {
+             Console.WriteLine("İşlem Tamalandı");
+         }
+              Console.WriteLine("------------------");
+            try
+            {
+                int c = int.Parse(null);
+                
+            }
+            catch (ArgumentNullException ex)
+            {
+              Console.WriteLine("Boş değer girdiniz");
+              Console.WriteLine(ex);    
+            
+            }
+
+            Console.WriteLine("------------------");
+
+             try
+            {
+                
+                int d = int.Parse("test");
+            }
+              catch (FormatException ex )
+            {
+              Console.WriteLine("Veri tipi uygun değil");
+              Console.WriteLine(ex);    
+            
+            }
+
+              Console.WriteLine("------------------");
+
+             try
+            {
+                
+                int d = int.Parse("-55000000000");
+            }
+              catch (OverflowException ex )
+            {
+              Console.WriteLine("Çok küçük veya büyük değer girdiniz.");
+              Console.WriteLine(ex);    
+            
+            }
+           
+
+
+
+       }
+
+
+
+    }
+
+
+
+
+
+}
