3a315337efc6ffdcc6c2ecca8cc927a89d57a524
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-01-28 09:15:57 +0300

    while-foreach eklendi.

---
+++ while_foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace while_foreach
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //   // While 
+      //   Console.Write("Enter a number");
+      //   int number = int.Parse(Console.ReadLine());
+      //   int counter = 1;
+      //   int total = 0;
+      //   while (counter <= number)
+      //   {
+      //     total += counter;
+      //     counter++;
+      //   }
+      //   Console.WriteLine(total / number);
+
+      //   // While 2
+
+      //   char character = 'a';
+      //   while(character < 'z'){
+      //       Console.Write(character);
+      //       character++;
+      //   }
+
+      // ForEach
+
+      string[] cars = { "BMW", "Ford", "Nissan", "Toyota" };
+
+      foreach (var car in cars)
+      {
+        Console.WriteLine(car);
+      }
+    }
+  }
+}
