9102f37202dcbb764c96e733a9618e30576d9793
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-01-28 09:30:10 +0300

    array methods eklendi.

---
+++ array_methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace array_methods
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Sort
+
+      int[] numberArray = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+      Console.WriteLine("------ Unordered Array ----");
+      foreach (var number in numberArray)
+        Console.WriteLine(number);
+
+      Console.WriteLine("------- Ordered List ------");
+      Array.Sort(numberArray);
+
+      foreach (var number in numberArray)
+        Console.WriteLine(number);
+
+      // Clear
+
+      Console.WriteLine("----- Array Clear -----");
+      Array.Clear(numberArray, 2, 2);
+
+      foreach (var number in numberArray)
+        Console.WriteLine(number);
+
+      // Reverse
+
+      Console.WriteLine("----- Array Reverse -----");
+      Array.Reverse(numberArray);
+
+      foreach (var number in numberArray)
+        Console.WriteLine(number);
+
+      // IndexOf
+
+      Console.WriteLine("------ Array IndexOf ------");
+      Console.WriteLine(Array.IndexOf(numberArray, 23));
+
+      // Resize
+
+      Console.WriteLine("----- Array Resize -----");
+      Array.Resize<int>(ref numberArray, 9);
+      numberArray[8] = 99;
+
+      foreach (var number in numberArray)
+        Console.WriteLine(number);
+    }
+  }
+}
