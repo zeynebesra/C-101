1663b6f76eed94e06dfc5a55e8c50d5d08d5284a
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-01-29 16:31:00 +0300

    Added enum and struct

---
+++ class/enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace enum
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Gunler.Pazar);
+      Console.WriteLine((int)Gunler.Cumartesi);
+
+      int sıcaklık = 32;
+
+      if (sıcaklık <= (int)HavaDurumu.Normal)
+      {
+        Console.WriteLine("Dışarıya çıkmak için havanın ısınması lazım");
+      }
+      else if (sıcaklık >= (int)HavaDurumu.CokSıcak)
+      {
+        Console.WriteLine("Hava dışarıya çıkmak için çok sıcak");
+      }
+      else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık < (int)HavaDurumu.CokSıcak)
+      {
+        Console.WriteLine("Hadi dışarıya çıkalım");
+      }
+    }
+  }
+
+  enum Gunler
+  {
+    Pazartesi = 1,
+    Salı,
+    Carsamba,
+    Persembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+  }
+
+  enum HavaDurumu
+  {
+    Soguk = 5,
+    Normal = 20,
+    Sıcak = 25,
+    CokSıcak = 30
+  }
+}
---
+++ class/struct/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace struct
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Dikdortgen dikdortgen = new Dikdortgen(); // new keywordu olmadan tanımlanamaz
+      dikdortgen.KisaKenar = 3;
+      dikdortgen.UzunKenar = 4;
+
+      Console.WriteLine("Class Alan Hesabı: {0}", dikdortgen.AlanHesapla()); // KisaKenar ve UzunKenar değerlerine değer atanmamış olsa bile AlanHesapla fonksiyonu çağrılabilir. Çünkü biz değer atamasak bile nesne oluşturulduğunda integer fieldlar default olarak 0 değerini alır.
+
+      // Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct(); // new keywordu olmadan da tanımlanabilir. Alttaki ilk 3 satır gibi
+
+      // Dikdortgen_Struct dikdortgen_Struct;
+      // dikdortgen_Struct.KisaKenar = 3;
+      // dikdortgen_Struct.UzunKenar = 4;
+
+      // Aşağıdaki satır parametreli constructor kullanımını örneklemek için 
+      Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct(3, 4);
+
+      Console.WriteLine("Struct Alan Hesabı: {0}", dikdortgen_Struct.AlanHesapla()); // KisaKenar ve UzunKenar değerlerine değer atanmamış olduğunda AlanHesapla fonksiyonu çağrılamaz. Bizim bir şekilde değer atamış olmamız gerekiyor.
+    }
+  }
+
+  class Dikdortgen
+  {
+    public int KisaKenar;
+    public int UzunKenar;
+
+    public Dikdortgen()
+    {
+      KisaKenar = 3;
+      UzunKenar = 4;
+    }
+    // Yukarıdaki gibi parametresiz constructor(Kurucu fonksiyon) tanımlanabilir.
+
+    public long AlanHesapla()
+    {
+      return this.KisaKenar * this.UzunKenar;
+    }
+  }
+
+  struct Dikdortgen_Struct
+  {
+    public int KisaKenar;
+    public int UzunKenar;
+
+    // public Dikdortgen_Struct()
+    // {
+    //     KisaKenar = 3;
+    //     UzunKenar = 4;
+    // } 
+    // Yukarıdaki gibi parametresiz constructor(Kurucu fonksiyon) tanımlanamaz.
+
+    public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+    {
+      KisaKenar = kisaKenar;
+      UzunKenar = uzunKenar;
+    }
+    // Constructor yukarıdaki gibi parametreli tanımlanmalıdır.
+
+    public long AlanHesapla()
+    {
+      return this.KisaKenar * this.UzunKenar;
+    }
+  }
+}
